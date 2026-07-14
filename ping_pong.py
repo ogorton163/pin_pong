a6d4eab3ad2dab494d59bd8922e8f0c4c4ee37e6
ogorton163 <galo4kin.petr.pavlovi4@gmail.com>
2026-07-14 13:09:03 +0400

    ping-pong v1.2
    добавленные обекты фон, 2 ракетки, управление ракетками

--- ping_pong.py
+++ ping_pong.py
@@ -2,14 +2,53 @@ from pygame import *
 
 window = display.set_mode((700, 500))
 display.set_caption("pig_pong")
-window.fill((110,90,255))
+king_fon = transform.scale(image.load("fon_pinpong.jpg"),(700,500))
 cloc = time.Clock()
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_left(self):
+        keys = key.get_pressed()
+
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+    def update_right(self):
+        keys = key.get_pressed()
+
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
+
+player_1 = Player("r_1.png", 670, 150, 1, 30, 150)
+player_2 = Player("r_2.png", 10, 150, 1, 30, 150)
 
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    window.blit(king_fon,(0,0))
+
+    player_1.update_left()
+    player_2.update_right()
+
+    player_1.reset()
+    player_2.reset()
 
     cloc.tick()
     display.update()
